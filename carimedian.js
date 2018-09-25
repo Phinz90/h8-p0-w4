1eace48966e1da2132033f91c693aaecc41dba4d
Yosephine Mellyana <42544640+Phinz90@users.noreply.github.com>
2018-09-25 15:19:18 +0700

    Update carimedian.js

--- carimedian.js
+++ carimedian.js
@@ -1,22 +1,17 @@
 // Diberikan sebuah function cariMedian(arr) yang menerima sebuah array angka. Function akan me-return median dari deret angka tersebut. Median adalah nilai tengah dari sebuah deret bilangan. Contoh, median atau dari [1, 2, 3, 4, 5] adalah 3 yang merupakan nilai yang ada di posisi tengah dari deret tersebut. Median dari deret yang berjumlah genap adalah rata-rata dari dua nilai tengah. Contoh, median dari [1, 2, 3, 4] adalah 2.5, karena (2 + 3 / 2).
 
 
- function cariMedian(arr) {
-	// you can only write your code here!
-	var ganjil = Math.round((arr.length + 1) / 2);
-	//console.log('Ganjil : ' +ganjil);
-
-	if (arr.length % 2 === 1) {
-		return arr[ganjil - 1];
-	} else {
-		var genap1 = arr.length / 2; //contoh soal 2 : 3
-		var genap2 = arr.length / 2 + 1; //contoh soal 2 : 4
-		var totalGenap = (arr[genap1 - 1] + arr[genap2 - 1]) / 2; //contoh soal 2 : arr[2] + arr[3]
-		//console.log('Genap1 : ' +genap1);
-		//console.log('Genap2 : ' +genap1);
-		//console.log('Genap : ' +totalGenap);
-		return totalGenap;
-	}
+function cariMedian(arr) {
+    var median = 0
+    var panjangArr = arr.length
+    if (arr.length % 2 !== 0) {
+        median = arr[Math.floor(panjangArr / 2)]
+    } else if (arr.length % 2 === 0) {
+        var nilaiAtas = arr[panjangArr / 2]
+        var nilaiBawah = arr[(panjangArr / 2) - 1]
+        median = (nilaiBawah + nilaiAtas) / 2
+    }
+	return median
 }
 
 // TEST CASES
